1225f22369ec46540df53933f5ad4fdbf06f15e1
GM1WKR <gm1wkr@gmail.com>
2022-06-29 22:00:27 +0100

    save

---
+++ src/components/GenreSelect.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const GenreSelect = ({ genres, onGenreSelected }) => {
+
+    const handleSelectChange = (event) => {
+        const selectedGenre = event.target.value;
+        onGenreSelected(selectedGenre);
+        console.log(selectedGenre);
+    }
+
+
+    const genreOptions = genres.map( (genre, index) => {
+        return(
+            <option key={index} value={genre.genreName}>{genre.genreName}</option>
+        )
+    });
+
+    return (
+        <select onChange={handleSelectChange}>
+            {genreOptions}
+        </select>
+    )
+}
+
+export default GenreSelect;
--- src/containers/GenreContainer.js
+++ src/containers/GenreContainer.js
@@ -1,10 +1,22 @@
 import React, {useState, useEffect} from "react";
 import GenreList from "../components/GenreList";
+import GenreSelect from "../components/GenreSelect";
 
 const GenreContainer = () => {
 
-    const [genreCharts, setGenreCharts] = useState([]);
+    const genreURLs = [
+        {genreName: "All", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"},
+        {genreName: "Rock", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=21/json"},
+        {genreName: "Dance", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=17/json"},
+        {genreName: "Country", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=6/json"}
+    ]
 
+
+    const [genreCharts, setGenreCharts] = useState([]);
+    const [genreURL, setGenreURL] = useState(genreURLs);
+    const [selectedGenre, setSelectedGenre] = useState(null);
+
+    console.log(selectedGenre);
 
     useEffect(() => {
         getGenreCharts();
@@ -20,6 +32,7 @@ const GenreContainer = () => {
         <>
             <section className="genre-container">
                 <h2>Genre Charts</h2>
+                <GenreSelect genres={genreURLs} onGenreSelected={setSelectedGenre} />
                 <GenreList genreCharts={genreCharts} />
             </section>
         
